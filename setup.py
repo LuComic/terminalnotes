bd311fe06e128b2432ff9e44be26f2185c43f94b
Lukas Jääger <81866926+LuComic@users.noreply.github.com>
2025-04-05 12:51:29 +0300

    some bug

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="termnotes",  # This is your package name
-    version="3.2",
+    version="3.3",
     packages=find_packages(),
     install_requires=[
         "termcolor",  # Add any dependencies you need here
