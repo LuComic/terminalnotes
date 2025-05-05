95d357c17b333ae995bb011ebe5e808e363e551f
Lukas Jääger <81866926+LuComic@users.noreply.github.com>
2025-05-06 00:52:25 +0300

    nothing changed

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="termnotes",  # This is your package name
-    version="17.9",
+    version="18.1",
     packages=find_packages(),
     install_requires=[
         "termcolor",  # Add any dependencies you need here
